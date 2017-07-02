7a84c3038bf2fe41e381c023bbdb5dfab80b16fa
Vali Shah <shaikanjavali.mastan@a-cti.com>
2017-07-02 10:15:09 +0530

    chore(releasing): Add travis config, conventional commit and semantic-release
    BREAKING CHANGE: closes #1

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+var starWars = require('./index');
+var expect = require('chai').expect;
+
+describe('starwars--names', function(){
+    describe('all', function(){
+        it('should be an arry of strings', function(){
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+            function isArrayOfStrings(array){
+                return array.every(function(item){
+                    return typeof item === 'string';
+                })
+            }
+        });
+
+        it('should include `Luke Skywalker`', function(){
+            expect(starWars.all).to.include('Luke Skywalker');
+        });
+    });
+    describe('random', function(){
+        it('should return random name from starWars.all', function(){
+            var randomItem = starWars.random();
+            expect(starWars.all).to.include(randomItem);
+        });
+    });
+})
